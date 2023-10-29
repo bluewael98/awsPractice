3731da9ef48730324dc5ce73a0933a23fd2d157c
Wael Hourani <wael.business@outlook.com>
2023-10-29 12:10:30 +1100

    first commit

---
+++ greetMe/index.mjs
@@ -0,0 +1,21 @@
+const moment = require("moment");
+const greeting = {
+  en: "Hello",
+  fr: "Bonjour",
+  hi: "Namaste",
+  es: "Hola",
+  pt: "Ola",
+};
+
+export const handler = async (event, context) => {
+  let name = event.pathParameters.name;
+  let { lang, ...info } = event.queryStringParameters;
+
+  let message = `${greeting[lang] ? greeting[lang] : greeting["en"]} ${name}`;
+  let response = { message: message, info: info, timestamp: moment().unix() };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+};
---
+++ resizeImage/index.mjs
@@ -0,0 +1,11 @@
+// const im = require("imagemagick");
+// const fs = require("fs");
+// const os = require("os");
+// const uuidv4 = require("uuid/v4");
+// const { promisify } = require("util");
+// const AWS = require("aws-sdk");
+
+// AWS.config.update({ region: "us-west-2" });
+// const s3 = new AWS.S3();
+
+// export const handler = async (event) => {};
